19727d5a1a3bbbbb5fb28211f22ba55b00834df9
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 17:37:22 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,13 @@
-#def square_array(array)
-#  # your code here
-#  new_array = []
-#  array.each do |square|
-#    new_array << square ** 2
-#  end
-#  return new_array
-#end
-
-
 def square_array(array)
+  # your code here
+  new_array = []
+  array.each do |square|#    new_array << square ** 2
+  end
+  return new_array
+end
+
+
+def square_array_collect(array)
   # your code here
   array.collect do |square|
     square ** 2
